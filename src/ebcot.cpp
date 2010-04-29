99fd1394e5d47b18ffb34ac5623ae58d62808267
argelast <argelast@8c8b5cc5-ec6d-4210-b25c-2bbcef93284f>
2010-04-29 16:28:43 +0000

    empezamos a hacer cositas

--- src/ebcot.cpp
+++ src/ebcot.cpp
@@ -4,8 +4,132 @@
 
 using namespace std;
 
+int matrizContextoLL[3][3][5];
+
+int matrizContextoHL[3][3][5];
+
+int matrizContextoHH[3][3][5];
+
+void inicializa(){
+
+matrizContextoLL[0][0][0] = 0;
+
+matrizContextoLL[0][0][1] = 1;
+
+matrizContextoLL[0][0][2] = 2;
+matrizContextoLL[0][0][3] = 2;
+matrizContextoLL[0][0][4] = 2;
+
+matrizContextoLL[0][1][0] = 3;
+matrizContextoLL[0][1][1] = 3;
+matrizContextoLL[0][1][2] = 3;
+matrizContextoLL[0][1][3] = 3;
+matrizContextoLL[0][1][4] = 3;
+
+matrizContextoLL[0][2][0] = 4;
+matrizContextoLL[0][2][1] = 4;
+matrizContextoLL[0][2][2] = 4;
+matrizContextoLL[0][2][3] = 4;
+matrizContextoLL[0][2][4] = 4;
+
+matrizContextoLL[1][0][0] = 5;
+
+matrizContextoLL[1][0][1] = 6;
+matrizContextoLL[1][0][2] = 6;
+matrizContextoLL[1][0][3] = 6;
+matrizContextoLL[1][0][4] = 6;
+
+matrizContextoLL[1][1][0] = 7;
+matrizContextoLL[1][1][1] = 7;
+matrizContextoLL[1][1][2] = 7;
+matrizContextoLL[1][1][3] = 7;
+matrizContextoLL[1][1][4] = 7;
+
+matrizContextoLL[2][0][0] = 8;
+matrizContextoLL[2][0][1] = 8;
+matrizContextoLL[2][0][2] = 8;
+matrizContextoLL[2][0][3] = 8;
+matrizContextoLL[2][0][4] = 8;
+matrizContextoLL[2][1][0] = 8;
+matrizContextoLL[2][1][1] = 8;
+matrizContextoLL[2][1][2] = 8;
+matrizContextoLL[2][1][3] = 8;
+matrizContextoLL[2][1][4] = 8;
+matrizContextoLL[2][2][0] = 8;
+matrizContextoLL[2][2][1] = 8;
+matrizContextoLL[2][2][2] = 8;
+matrizContextoLL[2][2][3] = 8;
+matrizContextoLL[2][2][4] = 8;
+}
+
+int calculaContexto(int h, int v, int d, int subbanda){
+
+switch(subbanda){
+	case LL:
+	case LH:
+		return matrizContextoLL[h][v][d];
+	break;
+	case HL;
+
+	break;
+
+	case HH;
+
+
+
+	break;
+
+
+}
+
+
+}
+
+
+
+
 int main (int argc, char const *argv[])
 {
+ 	
+	int anchoB1,altoB1,nivel,subbanda;
+        char fichero[20]="test/lena.bl1";
+ 	char ficherosal[20]="test/ej.bi1";
+	int **BloqueEj;
+	
+	ReservaPlano(64,64,&BloqueEj);
+	
+	CargaBloque(fichero, &anchoB1,&altoB1, &nivel, &subbanda,BloqueEj);
+	
+	// Saber cuantos planos de bits necesitaremos
+
+	for(int i = 0; i < anchoB1; i++){
+		for(int j = 0; j < altoB1){
+			
+		}
+	}
+	//Crear los planos de bits rellenandolos
+
+
+	InicializaEscritura(ficherosal);
+	FILE *^FCtxt=fopen("ej.ct1","wb");
+	
+	for(int i=0;i<anchoB1;i++){
+		for(int j=0;j<altoB1;j++){
+
+
+		// Hacer las tres pasadas
+
+
+
+	
+   }
+}
+
+FinalizaEscritura();
+fclose(FCtxt);
+GuardaBloque(ficherosal,anchoB1,altoB1,nivel,subbanda,BloqueEj);        
+
+
 	cout<< "Esto algun dia ira";
 	return 0;
 }
